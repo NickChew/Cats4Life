f58df0c2208863d183630224b606f8f547af684f
Hannah Mullah <hannahmullah@hotmail.com>
2023-01-06 11:43:10 +0000

    data fetched from cat api, installed faker npm and imported fake cat data

--- src/App.js
+++ src/App.js
@@ -3,32 +3,75 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Character from './Pages/Cats';
 import { useState, useEffect } from 'react';
-api_key=live_ykD4IvUF2Vht4ALDZOSGTY2fQRrYvtu4EZv3lOnTUiP3jZgQYUq8PjGW2EHdwmIK
+import { faker } from '@faker-js/faker'
+// api_key=live_ykD4IvUF2Vht4ALDZOSGTY2fQRrYvtu4EZv3lOnTUiP3jZgQYUq8PjGW2EHdwmIK
 
 function App() {
-    
   const [allCats, setAllCats] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(false);
 
   useEffect(() => {
-      const fetchData = async () => {    
-          const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
-          const data = await response.json();        
-          setAllCats(data);            
+    const fetchCatData = async () => {
+      try {
+        const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        const data = await response.json()
+        const catData = data.map((cat, index) => { //for every image in the cat array, do this and it should be different
+          return {
+            catId: index,
+            catImg: cat.url,
+            name: faker.name.findName(),
+            breed: faker.finance.amount(100, 300),
+            gender: faker.name.gender(),
+            location: faker.address.country()
+          }
+        })
+        setAllCats(catData);
+      } catch (err) {
+        setErrorMsg(err.errorMsg)
+        console.log(errorMsg)
       }
-  fetchData(); 
-  }, []);
+    }
+    fetchCatData();
+  }, [])
 
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>       
-      </nav>
-      <Routes>
-        <Route path='/' element={ <Home charArr={allCats} />} />
-        <Route path='/character/:id' element={ <Character />} />        
-      </Routes>
-      <h1>Footer</h1>
-    </BrowserRouter>
+
+    <div className="App">
+      {allCats.map((cat, index) => {
+        return (
+          <div>
+
+            <h3> Add to cart</h3>
+            <img src={cat.catImg} alt={cat.name}></img>
+            <h4>{cat.name}</h4>
+            <p>£{cat.price}</p>
+            <p>{cat.gender}</p>
+
+          </div>
+        )
+      })}
+
+    </div>
+
+
+
+
+
+
+
+    // <BrowserRouter>
+    //   <nav>
+    //     <Link to="/">Home</Link>
+    //   </nav>
+    //   <Routes>
+    //     <Route path='/' element={<Home charArr={allCats} />} />
+    //     <Route path='/character/:id' element={<Character />} />
+    //   </Routes>
+    //   <h1>Footer</h1>
+    // </BrowserRouter>
   );
 }
 
