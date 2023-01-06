1fcd59261579c7a81b6dda11b3bb470734ec782a
Hannah Mullah <hannahmullah@hotmail.com>
2023-01-06 13:50:45 +0000

    completed modal functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Home from './Pages/Home';
 import Character from './Pages/Cats';
 import { useState, useEffect } from 'react';
 import { faker } from '@faker-js/faker'
+import Modal from "./Modal"
+
 // api_key=live_ykD4IvUF2Vht4ALDZOSGTY2fQRrYvtu4EZv3lOnTUiP3jZgQYUq8PjGW2EHdwmIK
 
 function App() {
   const [allCats, setAllCats] = useState([]);
   const [errorMsg, setErrorMsg] = useState(false);
+
+  const [currentCat, setCurrentCat] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchCatData = async () => {
@@ -22,8 +27,9 @@ function App() {
           return {
             catId: index,
             catImg: cat.url,
-            name: faker.name.findName(),
-            breed: faker.finance.amount(100, 300),
+            name: faker.name.fullName(),
+            price: faker.finance.amount(100, 300),
+            breed: faker.animal.cat(),
             gender: faker.name.gender(),
             location: faker.address.country()
           }
@@ -37,27 +43,33 @@ function App() {
     fetchCatData();
   }, [])
 
+  const handleClick = (catus) => {
+    setCurrentCat(catus);
+    setShowModal(true)
+  }
+
+
+
   return (
 
     <div className="App">
-      <div className='Wrapper'>
-        <div className='catData' >
-          {allCats.map((cat, index) => {
-            return (
-              <div className='catInfo'>
-                <h3> Add to cart</h3>
-                <img src={cat.catImg} alt={cat.name} height='250px' width='250px'></img>
-                <h4>{cat.name}</h4>
-                <p>£{cat.price}</p>
-                <p>{cat.gender}</p>
-                <p>Breed : {cat.breed}</p>
-                <p>Location: {cat.location}</p>
-              </div>
 
-            )
-          })}
-        </div >
-      </div>
+      <div className='catData' >
+        {allCats.map((cat, index) => {
+          return (
+
+            <div className='catInfo'>
+              <h3> Add to cart</h3>
+              <img key={index} src={cat.catImg} alt={cat.name} height='250px' width='250px' onClick={() => handleClick(cat)} />
+            </div>
+
+          )
+        })}
+      </div >
+
+      {console.log(currentCat)}
+      {showModal && <Modal closeModal={setShowModal} cat={currentCat}></Modal>}
+
     </div >
 
 
