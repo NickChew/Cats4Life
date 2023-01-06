2cba1b17fad2334822f6577092bb2367ea03d667
Sobana Qadus <sobanaq@hotmail.com>
2023-01-06 12:33:38 +0000

    final commit to navbarSobana

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './Pages/Home';
-import Character from './Pages/Cats';
-import { useState, useEffect } from 'react';
-api_key=live_ykD4IvUF2Vht4ALDZOSGTY2fQRrYvtu4EZv3lOnTUiP3jZgQYUq8PjGW2EHdwmIK
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import Character from "./Pages/Cats";
+import { useState, useEffect } from "react";
+import logo from "./Cats4Life.png";
 
 function App() {
-    
   const [allCats, setAllCats] = useState([]);
 
   useEffect(() => {
-      const fetchData = async () => {    
-          const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
-          const data = await response.json();        
-          setAllCats(data);            
-      }
-  fetchData(); 
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.thecatapi.com/v1/images/search?limit=10"
+      );
+      const data = await response.json();
+      setAllCats(data);
+    };
+    fetchData();
   }, []);
 
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>       
-      </nav>
+      <div className="navBar">
+        <nav>
+          <img id="logo" src={logo} alt="Cats4Life logo"></img>
+          <Link to="/">Home</Link>
+          <Link to="/Pages/About.js">About us</Link>
+          <Link to="/">Shop</Link>
+          <Link to="/Pages/PetAdvice.js">Pet advice</Link>
+          <Link to="/Pages/Contact.js">Contact</Link>
+        </nav>
+      </div>
+
       <Routes>
-        <Route path='/' element={ <Home charArr={allCats} />} />
-        <Route path='/character/:id' element={ <Character />} />        
+        <Route path="/" element={<Home charArr={allCats} />} />
+        <Route path="/character/:id" element={<Character />} />
       </Routes>
       <h1>Footer</h1>
     </BrowserRouter>
